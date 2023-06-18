9b0500c2ef0f8e6bb6832302a988ea5301a8bc59
m-j-3 <114370635+m-j-3@users.noreply.github.com>
2023-06-18 11:40:48 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -13,38 +13,34 @@ c.execute("""
     CREATE TABLE IF NOT EXISTS messages (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         user_id TEXT NOT NULL,
+        sender TEXT NOT NULL,
         message TEXT NOT NULL
     )
 """)
 conn.commit()
 
+def store_message(user_id, sender, message):
+    # Insert the message into the database
+    c.execute("INSERT INTO messages (user_id, sender, message) VALUES (?, ?, ?)", (user_id, sender, message))
+    conn.commit()
+
 @app.route("/messages/<string:user_id>", methods=["POST"])
 def add_message(user_id):
     data = request.get_json(force=True)
     message = data["message"]
-    
-    # Insert the message into the database
-    c.execute("INSERT INTO messages (user_id, message) VALUES (?, ?)", (user_id, message))
-    conn.commit()
-    
+    store_message(user_id, "user", message)  # Store the user's message
+    # ... Additional logic to process the user's message and generate a response from ChatGPT ...
+    response = "This is ChatGPT's response"  # Replace with the actual response from ChatGPT
+    store_message(user_id, "chatgpt", response)  # Store ChatGPT's response
     return Response(response="OK", status=200)
 
 @app.route("/messages/<string:user_id>", methods=["GET"])
 def get_messages(user_id):
     # Retrieve messages from the database for the given user_id
-    c.execute("SELECT message FROM messages WHERE user_id=?", (user_id,))
-    messages = [row[0] for row in c.fetchall()]
-    
+    c.execute("SELECT sender, message FROM messages WHERE user_id=?", (user_id,))
+    rows = c.fetchall()
+    messages = [{"sender": row[0], "message": row[1]} for row in rows]
     return Response(response=json.dumps(messages), status=200)
-
-# ... Define other routes for logo, plugin manifest, and openapi ...
-
-# This key can be anything, though you will likely want a randomly generated sequence.
-_SERVICE_AUTH_KEY = "REPLACE_ME"
-
-def assert_auth_header():
-    auth_header = request.headers.get("Authorization")
-    assert auth_header == f"Bearer {_SERVICE_AUTH_KEY}"
 
 @app.route("/logo.png")
 def plugin_logo():
