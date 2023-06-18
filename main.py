f61fef318e29d8307592fa42f9d426cd94827b56
m-j-3 <114370635+m-j-3@users.noreply.github.com>
2023-06-18 11:31:05 -0500

    modified:   main.py     new file:   messages.db

--- main.py
+++ main.py
@@ -1,30 +1,50 @@
+import sqlite3
 import json
 from flask import Flask, request, Response, send_file
 
 app = Flask(__name__)
 
-# This key can be anything, though you will likely want a randomly generated sequence.
-_SERVICE_AUTH_KEY = "REPLACE_ME"
-_MESSAGES = {}
+# Connect to the SQLite database
+conn = sqlite3.connect("messages.db")
+c = conn.cursor()
 
-def assert_auth_header():
-    auth_header = request.headers.get("Authorization")
-    assert auth_header == f"Bearer {_SERVICE_AUTH_KEY}"
+# Create a table for storing messages
+c.execute("""
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id TEXT NOT NULL,
+        message TEXT NOT NULL
+    )
+""")
+conn.commit()
 
 @app.route("/messages/<string:user_id>", methods=["POST"])
 def add_message(user_id):
-    assert_auth_header()
     data = request.get_json(force=True)
-    if user_id not in _MESSAGES:
-        _MESSAGES[user_id] = []
-    _MESSAGES[user_id].append(data["message"])
+    message = data["message"]
+    
+    # Insert the message into the database
+    c.execute("INSERT INTO messages (user_id, message) VALUES (?, ?)", (user_id, message))
+    conn.commit()
+    
     return Response(response="OK", status=200)
 
 @app.route("/messages/<string:user_id>", methods=["GET"])
 def get_messages(user_id):
-    assert_auth_header()
-    messages = _MESSAGES.get(user_id, [])
+    # Retrieve messages from the database for the given user_id
+    c.execute("SELECT message FROM messages WHERE user_id=?", (user_id,))
+    messages = [row[0] for row in c.fetchall()]
+    
     return Response(response=json.dumps(messages), status=200)
+
+# ... Define other routes for logo, plugin manifest, and openapi ...
+
+# This key can be anything, though you will likely want a randomly generated sequence.
+_SERVICE_AUTH_KEY = "REPLACE_ME"
+
+def assert_auth_header():
+    auth_header = request.headers.get("Authorization")
+    assert auth_header == f"Bearer {_SERVICE_AUTH_KEY}"
 
 @app.route("/logo.png")
 def plugin_logo():
